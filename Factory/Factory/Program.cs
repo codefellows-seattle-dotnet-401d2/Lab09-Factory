3496b407b1c08eeb8d0f71824881752171298df1
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-04 09:57:09 -0800

    Initial Setup

---
+++ Factory/Factory/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+/*
+TODO: This program should consist of a document/resume creation factory.
+TODO:  You are creating types of Documents that make up a Resume or Report, that conists of different Pages such as Cover Letter, References, Work History, Summary Page etc...
+TODO: Identify the following. In your readme (hint: i bolded some of the important keywords)
+TODO: Product --
+TODO: Concrete Product --
+TODO: Creator --
+TODO: Concrete Creator --
+TODO: In your Program.cs file should clearly show through Console.WriteLines() that your program is functioning as requried.
+*/
+
+namespace Factory
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static string  Test()
+        {
+            return "1";
+        }
+    }
+}
