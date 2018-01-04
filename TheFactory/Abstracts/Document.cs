1ab1f0181bbd8bc9a7dc5d79fee0b3d344c15837
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-01-04 14:27:36 -0800

    Derp upload!

---
+++ TheFactory/Abstracts/Document.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheFactory.Abstracts
+{
+    public abstract class Document
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ TheFactory/Concretes/TheFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TheFactory.Abstracts;
+using TheFactory.Concretes;
+
+namespace TheFactory.Concretes
+{
+    public class TheFactory : Factory
+    {
+        public SCPDocument PrintSCP(string title, string scproceedures, string description)
+        {
+            SCPDocument Scip = new SCPDocument(title,scproceedures,description);
+        }
+
+        public TheFactory(string _user)
+        {
+            user = _user;
+        }
+    }
+}
