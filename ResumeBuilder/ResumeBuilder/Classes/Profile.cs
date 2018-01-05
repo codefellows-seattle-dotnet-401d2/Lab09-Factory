2a81a411ca27df8fe80e69b19597cfd2fe886e50
Luay <luayghazwan@yahoo.com>
2018-01-05 13:14:51 -0800

    Profile Class implemented with necessary properties

--- ResumeBuilder/ResumeBuilder/Classes/Profile.cs
+++ ResumeBuilder/ResumeBuilder/Classes/Profile.cs
@@ -6,5 +6,9 @@ namespace ResumeBuilder.Classes
 {
     public class Profile : Page
     {
+        public string ApplicantName { get; set; }
+        public string Email { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Summary { get; set; }
     }
 }
