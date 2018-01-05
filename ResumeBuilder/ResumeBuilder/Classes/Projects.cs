6856eeff4ea0f3b2559b35fecd80b95b159a85dd
Luay <luayghazwan@yahoo.com>
2018-01-05 13:17:55 -0800

    Project class implemented

--- ResumeBuilder/ResumeBuilder/Classes/Projects.cs
+++ ResumeBuilder/ResumeBuilder/Classes/Projects.cs
@@ -6,5 +6,8 @@ namespace ResumeBuilder.Classes
 {
     public class Projects : Page
     {
+        public string ProjectName { get; set; }
+        public string Description { get; set; }
+        public int TotalMonthsSpent { get; set; }
     }
 }
