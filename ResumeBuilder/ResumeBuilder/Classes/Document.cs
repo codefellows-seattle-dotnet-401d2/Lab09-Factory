e853c67f47e06a168014fe59f493af8b7540fa3f
Luay <luayghazwan@yahoo.com>
2018-01-05 11:46:50 -0800

    Creator class implemented

--- ResumeBuilder/ResumeBuilder/Classes/Document.cs
+++ ResumeBuilder/ResumeBuilder/Classes/Document.cs
@@ -6,6 +6,20 @@ namespace ResumeBuilder.Classes
 {
     abstract class Document
     {
-        
+        // List of Products:Pages
+        private List<Page> _pages = new List<Page>();
+
+        public Document()
+        {
+            CreatePage();
+        }
+
+        // Factory Method
+        public abstract void CreatePage();
+
+        public List<Page> GetPages
+        {
+            get { return _pages; }
+        }
     }
 }
