23a03c31508f8717b832225b80c70ee20aa3accb
Luay <luayghazwan@yahoo.com>
2018-01-05 11:43:42 -0800

    Scaffolding for the resume Factory design

---
+++ ResumeBuilder/ResumeBuilder/Classes/WorkHistory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ResumeBuilder.Classes
+{
+    class WorkHistory : Page
+    {
+    }
+}
---
+++ ResumeBuilder/ResumeBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ResumeBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
