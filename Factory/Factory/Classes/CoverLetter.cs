293e467b9ca484b2f15f9bc980d997712361bc4b
jeffxmartinez <jeffxmartinez@gmail.com>
2018-01-06 17:58:40 -0800

    updated two seperate resumes

---
+++ Factory/Factory/Classes/CoverLetter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory
+{
+    /// <summary>
+    /// Concrete Product
+    /// </summary>
+    public class CoverLetter : Pages
+    {
+        public CoverLetter()
+        {
+            Console.WriteLine("Cover Letter for Microsoft");
+        }
+    }
+}
+
---
+++ Factory/Factory/Classes/Document.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory.Classes
+{
+    /// <summary>
+    /// Creator
+    /// </summary>
+    public abstract class Document
+    {
+           private List<Pages> pages = new List<Pages>();
+
+            public Document()
+            {
+                CreateResume();
+            }
+
+            //Factory method
+            public abstract void CreateResume();
+
+            public List<Pages> Pages
+            {
+                get { return pages; }
+            }
+    }
+}
+
